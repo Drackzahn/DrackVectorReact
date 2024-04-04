7651bd819084fe2902c6eb9bcb3fd524b0fa4c8f
Drackzahn <Aiblis@gmx.net>
2024-04-04 20:20:39 +0200

    Fix duplicate

--- src/hooks/useDataContextHelper.tsx
+++ src/hooks/useDataContextHelper.tsx
@@ -1,46 +1,54 @@
 import { useState } from "react";
 import { IDataBase } from "../data/context/iDataContext";
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
 export function useDataContextHelper<T extends IDataBase>() {
-    const [datas, setdatas] = useState<T[]>([]);
-    const [selected, setSelected] = useState<T | null>(null);
+  const [datas, setdatas] = useState<T[]>([]);
+  const [selected, setSelected] = useState<T | null>(null);
 
-    function add(value: T) {
-        const newGrounds = datas.concat(value);
-        setdatas(newGrounds);
-        setSelected(value);
-    }
+  function add(value: T) {
+    const newGrounds = datas.concat(value);
+    setdatas(newGrounds);
+    setSelected(value);
+  }
 
-    function updateSelected(value: T) {
-        const data = datas.map(x => {
-            if (x.id === value.id) {
-                return value;
-            }
+  function updateSelected(value: T) {
+    const data = datas.map((x) => {
+      if (x.id === value.id) {
+        return value;
+      }
 
-            return x;
-        })
+      return x;
+    });
 
-        setdatas(data);
-        setSelected(value);
-    }
+    setdatas(data);
+    setSelected(value);
+  }
 
-    function deleteSelected(value: T) {
-        const data = datas.filter(x => x.id !== value.id);
+  function deleteSelected(value: T) {
+    const data = datas.filter((x) => x.id !== value.id);
 
-        setdatas(data);
-        setSelected(null);
-    }
+    setdatas(data);
+    setSelected(null);
+  }
 
-    function duplicate(value: T) {
-        value.id = uuidv4();
-        setdatas(datas.concat(value));
-        setSelected(value);
-    }
+  function duplicate(value: T) {
+    const duplicate = { ...value, id: uuidv4() };
+    setdatas(datas.concat(duplicate));
+    setSelected(duplicate);
+  }
 
-    function select(value: T) {
-        setSelected(value);
-    }
+  function select(value: T) {
+    setSelected(value);
+  }
 
-    return [datas, selected, add, updateSelected, deleteSelected, duplicate, select] as const;
+  return [
+    datas,
+    selected,
+    add,
+    updateSelected,
+    deleteSelected,
+    duplicate,
+    select,
+  ] as const;
 }
